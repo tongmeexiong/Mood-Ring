6afada9e76e3d917fb1acdbfb375c6e58651e761
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-05-17 16:24:17 -0500

    added routers in server

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const tagsRouter = require('./modules/Routes/tags.router')
+const imagesRouter = require('./modules/Routes/images.router')
+
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
+app.use('/api/tags', tagsRouter)
+app.use('/api/images', imagesRouter)
 
 
 /** ---------- START SERVER ---------- **/
